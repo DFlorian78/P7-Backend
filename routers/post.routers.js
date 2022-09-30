d36dc25c46718f368e6749156fe4a458b6cd7e3b
FlorianD <100361045+DFlorian78@users.noreply.github.com>
2022-10-01 00:21:18 +0200

    Add files via upload

--- routers/post.routers.js
+++ routers/post.routers.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { getPosts, createPost, createComment, deletePost, likePost } = require('../controllers/posts')
+const { getPosts, createPost, createComment, deletePost, likePost, updatePost } = require('../controllers/posts')
 const { checkToken} = require('../middleware/token')
 const { imagesUpload} = require('../middleware/images')
 const postRouter = express.Router()
@@ -7,9 +7,10 @@ const postRouter = express.Router()
 
 postRouter.use(checkToken)
 postRouter.post("/:id", createComment)
-postRouter.delete("/:id", deletePost)
 postRouter.get("/", getPosts)
 postRouter.post("/", imagesUpload, createPost)
+postRouter.put("/:id", imagesUpload, updatePost)
+postRouter.delete("/:id", deletePost)
 postRouter.post("/:id/like", likePost)
 //postRouter.put("/:id", upload.single("image"), modifyPost)
 
